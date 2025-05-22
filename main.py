2889311465cf9e3893390d2b684980020aece95d
desireeshortiee <desireeganohay13@gmail.com>
2025-05-22 17:23:05 +0800

    asdsad

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+import models
+import schemas
+from database import engine, SessionLocal, Base
 from fastapi.middleware.cors import CORSMiddleware
-
-from . import models, schemas
-from .database import engine, SessionLocal, Base
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]  # Frontend origin
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -31,9 +28,8 @@ def read_todos(db: Session = Depends(get_db)):
     return db.query(models.JazelTodo).all()
 
 @app.get("/jazeltodo/{todo_id}", response_model=schemas.JazelTodoOut)
-
 def read_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(models.JazelTodo).filter(models.JazelTodo.id == todo_id).first()
+    todo = db.query(models.JazelTodo).get(todo_id)
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
@@ -48,7 +44,7 @@ def create_todo(todo: schemas.JazelTodoCreate, db: Session = Depends(get_db)):
 
 @app.put("/jazeltodo/{todo_id}", response_model=schemas.JazelTodoOut)
 def update_todo(todo_id: int, todo: schemas.JazelTodoUpdate, db: Session = Depends(get_db)):
-    db_todo = db.query(models.JazelTodo).filter(models.JazelTodo.id == todo_id).first()
+    db_todo = db.query(models.JazelTodo).get(todo_id)
     if not db_todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     for key, value in todo.dict().items():
@@ -59,7 +55,7 @@ def update_todo(todo_id: int, todo: schemas.JazelTodoUpdate, db: Session = Depen
 
 @app.delete("/jazeltodo/{todo_id}")
 def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    db_todo = db.query(models.JazelTodo).filter(models.JazelTodo.id == todo_id).first()
+    db_todo = db.query(models.JazelTodo).get(todo_id)
     if not db_todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     db.delete(db_todo)
